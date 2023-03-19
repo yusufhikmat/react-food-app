e5b6e17557b3a51c4ee2d20e2da23b2e460246d7
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-03-19 00:02:37 -1200

    ...

--- src/components/carts/Cart.js
+++ src/components/carts/Cart.js
@@ -2,14 +2,26 @@ import React, { useContext } from 'react'
 import CartContext from '../../store/CartContext'
 import Modal from '../shared/modal/Modal'
 import classes from './Cart.module.css'
+import CartItem from './CartItem'
 const Cart = ({hideCart}) => {
      const cartOutput = useContext(CartContext)
 
      const totalAmount = `$${cartOutput.totalAmount.toFixed(2)}`
     const availableItems = cartOutput.items.length > 0;
+
+    const cartItemRemoveHandler=(id)=>{}
+    const cartItemAddHandler =(item)=>{}
     const cartItems = <ul className={classes['cart-items']}>{
         cartOutput.items.map((item)=>{
-        return <li key={item.id}>{item.name}</li>
+        return <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price} 
+                    cartItemRemoveHandler={cartItemRemoveHandler.bind(null, item.id)}
+                    cartItemAddHandler = {cartItemAddHandler.bind(null,item)}
+
+                    />
     })
     }
     </ul>
